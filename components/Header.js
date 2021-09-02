6af0c68776e0b82fb4b077ff56a880e79ad8e755
Royal6969 <sergiodiazcampos@gmail.com>
2021-09-02 22:41:54 +0200

    implement DataPicker in Header Component

--- components/Header.js
+++ components/Header.js
@@ -6,9 +6,36 @@ import {
     SearchIcon,
     UsersIcon,
 } from '@heroicons/react/solid'; //npm install @heroicons/react
+import { useState } from "react";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 function Header() {
-    return (
+
+    //una vez acabado el index.js (homeScreen), vamos ahora ha hacer el Calendar y Search 
+    //para ello necesitamos usar el State del input del Header
+    const [searchInput, setSearchInput] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [numberOfGuest, setNumberOfGuest] = useState(1);
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    };
+
+    const resetInput = () => {
+        setSearchInput("");
+    };
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection'
+    };
+
+    return ( //esto fue lo 1º
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
 
             {/* left */}
@@ -24,6 +51,8 @@ function Header() {
             {/* Middle - Search */}
             <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
                 <input 
+                    value={searchInput} //conecting the useState
+                    onChange={(e) => setSearchInput(e.target.value)} //conecting the useState
                     className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" 
                     type="text" 
                     placeholder="Start your search" 
@@ -41,6 +70,43 @@ function Header() {
                     <UserCircleIcon className="h-6" />
                 </div>
             </div>
+
+            {/* npm install --save react-date-range */}
+            {/* npm install --save react date-fns */}
+            {searchInput && (
+                <div className="flex flex-col col-span-3 mx-auto">
+                    <DateRangePicker 
+                        ranges={[selectionRange]} 
+                        minDate={new Date()}
+                        rangeColors={["#FD5861"]}
+                        onChange={handleSelect}
+                    />
+                    <div className="flex items-center border-b mb-4">
+                        <h2 className="text-2xl flex-grow font-semibold">
+                            Number of Guest
+                        </h2>
+                        
+                        <UsersIcon className="h-5" />
+                        <input
+                            value={numberOfGuest}
+                            onChange={(e) => setNumberOfGuest(e.target.value)}
+                            type="number" 
+                            min={1}
+                            className="w-12 pl-2 text-lg outline-none text-red-400" 
+                        />
+                    </div>
+                    <div className="flex">
+                        <button 
+                            className="flex-grow text-gray-500"
+                            onClick={resetInput}
+                        >
+                            Cancel
+                        </button>
+                        <button className="flex-grow text-red-400">Search</button>
+                    </div>
+                </div>
+            )}
+            
         </header>
     );
 }
